cdeae4b2aa94cd673d38fd193b48aa1498b078bf
Clem-svg <clementine.fouque@gmail.com>
2022-02-14 16:20:23 +0100

    vuln code v1

---
+++ script.js
@@ -0,0 +1,13 @@
+var userName = document.querySelector('input[name="userName"]')
+var userPassWord = document.querySelector('input[name="userPassword"]')
+var loginForm = document.getElementById('form')
+
+window.onload = init()
+
+function init() {
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        localStorage.setItem('myUserName', userName.value);
+        window.location = "welcome.html"
+    })
+}
